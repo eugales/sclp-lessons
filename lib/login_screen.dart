84d320fba417bee9a5c52ba90590e60a85e5e2ba
Adil <mamyrkhanov.adil@gmail.com>
2022-06-04 03:20:31 +0600

    login screen refactoring

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lessons2/home_screen.dart';
 
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -12,47 +13,30 @@ class _LoginScreenState extends State<LoginScreen> {
   final _loginController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  var _loginCounter = 0;
-  var _passwordCounter = 0;
+  final formKey = GlobalKey<FormState>();
 
-  void _loginCounterChanged(value) {
-    setState(() {
-      _loginCounter = value.length;
-    });
-  }
-
-  void _passwordCounterChanged(value) {
-    setState(() {
-      _passwordCounter = value.length;
-    });
-  }
-
-  String? _loginErrorText;
-  String? _passwordErrorText;
-
-  void _loginErrorValidator() {
-    if (_loginCounter < 3) {
-      _loginErrorText = 'Логин должен содержать не менее 3 символов';
-    } else {
-      _loginErrorText = null;
+  String? _loginValidator(String? value) {
+    if (value == null || value.isEmpty || value.length < 3) {
+      return 'Логин должен содержать не менее 3 символов';
     }
-    // setState(() {});
+    return null;
   }
 
-  void _passwordErrorValidator() {
-    if (_passwordCounter < 8) {
-      _passwordErrorText = 'Пароль должен содержать не менее 8 символов';
-    } else {
-      _passwordErrorText = null;
+  String? _passwordValidator(String? value) {
+    if (value == null || value.isEmpty || value.length < 8) {
+      return 'Пароль должен содержать не менее 8 символов';
     }
-    // setState(() {});
+    return null;
   }
 
   void _onLoginPressed() {
     FocusScope.of(context).unfocus();
-    _loginErrorValidator();
-    _passwordErrorValidator();
-    if (_loginErrorText != null || _passwordErrorText != null) return;
+
+    final isValidated = formKey.currentState?.validate() ?? false;
+
+    if (!isValidated) return;
+
+    formKey.currentState?.save();
 
     if (_loginController.text == 'qwerty' &&
         _passwordController.text == '123456ab') {
@@ -81,6 +65,10 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  void _onValueChanged(value) {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,69 +76,57 @@ class _LoginScreenState extends State<LoginScreen> {
         title: const Text('Авторизация'),
         centerTitle: false,
       ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 15, right: 15, bottom: 30),
-        child: Form(
-          child: Column(
-            children: [
-              Expanded(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 50),
+        child: Column(
+          children: [
+            Form(
+              key: formKey,
+              child: Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Flexible(
-                          child: Text(
-                            'Введите логин и пароль',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ],
+                    const Text(
+                      'Введите логин и пароль',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    Column(
-                      children: [
-                        TextFormField(
-                          controller: _loginController,
-                          onChanged: _loginCounterChanged,
-                          maxLength: 8,
-                          decoration: InputDecoration(
-                            label: const Text('Логин'),
-                            errorText: _loginErrorText,
-                            counter: Text('$_loginCounter/8'),
-                          ),
-                        ),
-                      ],
+                    TextFormField(
+                      controller: _loginController,
+                      maxLength: 8,
+                      validator: _loginValidator,
+                      onChanged: _onValueChanged,
+                      decoration: InputDecoration(
+                        label: const Text('Логин'),
+                        counter: Text(
+                            '${_loginController.text.length}/8'),
+                      ),
                     ),
-                    Column(
-                      children: [
-                        TextFormField(
-                          controller: _passwordController,
-                          onChanged: _passwordCounterChanged,
-                          maxLength: 16,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              label: const Text('Пароль'),
-                              errorText: _passwordErrorText,
-                              counter: Text('$_passwordCounter/16')),
-                        ),
-                      ],
+                    TextFormField(
+                      controller: _passwordController,
+                      maxLength: 16,
+                      validator: _passwordValidator,
+                      onChanged: _onValueChanged,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          label: const Text('Пароль'),
+                          counter: Text(
+                              '${_passwordController.text.length}/16')),
                     ),
                   ],
                 ),
               ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: _onLoginPressed,
-                      child: const Text('Вход'),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: _onLoginPressed,
+                    child: const Text('Вход'),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
